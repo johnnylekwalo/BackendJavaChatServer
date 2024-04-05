6d28a382b560ee3ce84872013449416ef8c1d84b
Johnny Lekwalo Leseyane <leseya99@mit.edu>
2024-04-05 14:25:55 +0200

    Initial Commit : Initialize a Spring Boot project , Spring Web, Spring Security, and WebSocket

---
+++ src/main/java/com/assessment/javachatserver/JavachatserverApplication.java
@@ -0,0 +1,13 @@
+package com.assessment.javachatserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavachatserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavachatserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assessment/javachatserver/JavachatserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assessment.javachatserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavachatserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
